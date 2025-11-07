7a153ab5920bb8eba5f2398273d3e5b6ea02d222
nguyenquan-2510 <quannguyenminhbl@gmail.com>
2025-11-07 16:44:03 +0700

    Ye how to write HTML

--- webtest.lua
+++ webtest.lua
@@ -1,10 +1,24 @@
 local js = js.global
+local document = js.document
 
 print("Hello world ! This is Lua code")
 
 js.console:log("Hello world ! This is JS code")
-js:alert("You write Lua code ?")
+-- js:alert("You write Lua code ?")
 
-local output = js.document:getElementById("output")
+local output = document:getElementById("output")
 
 output.textContent = "Hello world ! This line is written in Lua"
+
+
+-- HTML
+local root = document:getElementById("root")
+local html = [[
+
+<h2>Hello from Lua!</h2>
+<p>This HTML was inserted from Lua string.</p>
+<button id="btn">Click me</button>
+
+]]
+
+root.innerHTML = html
